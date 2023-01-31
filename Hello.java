e5173f903b17dafa538816d2243057acfc7fc68c
Nadika <sharman@lawrence.edu>
2023-01-31 10:02:50 -0600

    added code

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 class Hello{
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!"); //printing hello world
         // Hey there ;)
-        System.out.println("How are you?");
+        System.out.println("How are you?"); //printing how are you
+        System.out.println("Okay, bye!"); //saying bye
     }
 }
-
